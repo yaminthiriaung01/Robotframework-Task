c28b5d617c7be4b8b18fd48a0dc7c7a5b3804f5a
yaminthiriaung01 <123317690+yaminthiriaung01@users.noreply.github.com>
2023-01-23 03:26:32 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@
 set filename1=LoginLogout
 set filename2=ChangeMode
 set filename3=Search
-set filepath=%cd%\Tests\
+set filepath=%cd%/Tests/
 
 set CUR_YYYY=%date:~10,4%
 set CUR_MM=%date:~4,2%
@@ -15,6 +15,6 @@ set CUR_SS=%time:~6,2%
 
 set name=%CUR_DD%.%CUR_MM%.%CUR_YYYY%-%CUR_HH%.%CUR_NN%.%CUR_SS%
 
-robot --outputdir %cd%\Results\ResultsFor%filename1% %filepath%%filename1%.robot
-robot --outputdir %cd%\Results\ResultsFor%filename2% %filepath%%filename2%.robot
-robot --outputdir %cd%\Results\ResultsFor%filename3% %filepath%%filename3%.robot
+robot --outputdir %cd%/Results/ResultsFor%filename1% %filepath%%filename1%.robot
+robot --outputdir %cd%/Results/ResultsFor%filename2% %filepath%%filename2%.robot
+robot --outputdir %cd%/Results/ResultsFor%filename3% %filepath%%filename3%.robot
